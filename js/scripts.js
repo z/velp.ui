fb3a8de29092dfd74ba8ebf700006217c48879c6
Tyler '-z-' Mulligan <z@xnz.me>
2015-02-23 19:25:17 -0500

    matches are now in panels and data is condensed to 1 info table and 3 tabs

--- js/scripts.js
+++ js/scripts.js
@@ -9,75 +9,62 @@ $(function() {
     function addTo(type, data) {
       switch (type) {
         case 'gamestart':
-          match = Math.random().toString(36).substr(2, 5);;
-          $('#' + type).append(
-            '<li>' +
-              '<h4>Match ' + match + '</h4>' +
-              '<table class="table" id="gamestart-match-' + match + '">' +
-                '<tr>' +
-                  '<th>Map Name</th>' +
-                  '<th>Match ID</th>' +
-                  '<th>Game Type</th>' +
-                '</tr>' +
-              '</table>' +
-            '</li>'
+          match = Math.random().toString(36).substr(2, 5);
+          $("#matches").append(
+            '<div class="panel panel-primary">' +
+              '<div class="panel-heading collapser"><h3 class="panel-title"><a data-toggle="collapse" data-target="#match-' + match + '" href="#match-' + match + '">Match ' + match + '</a></h3></div>' +
+              '<div id="match-' + match + '" class="panel-collapse collapse in">' +
+                '<div class="panel-body">' +
+                  '<h4>Match ' + match + '</h4>' +
+                  '<table class="table game-info">' +
+                    '<tr>' +
+                      '<th>Map Name</th>' +
+                      '<th>Match ID</th>' +
+                      '<th>Game Type</th>' +
+                      '<th>Mutators</th>' +
+                    '</tr>' +
+                  '</table>' +
+                  '<ul class="nav nav-tabs" role="tablist">' +
+                    '<li role="presentation" class="active"><a href="#match-' + match + '-tab-1" aria-controls="tab-1" role="tab" data-toggle="tab">Players</a></li>' +
+                    '<li role="presentation"><a href="#match-' + match + '-tab-2" aria-controls="tab-2" role="tab" data-toggle="tab">Kills</a></li>' +
+                    '<li role="presentation"><a href="#match-' + match + '-tab-3" aria-controls="tab-3" role="tab" data-toggle="tab">Join / Part</a></li>' +
+                  '</ul>' +
+                  '<div class="col-md-12">' +
+                    '<div class="tab-content">' +
+                      '<div role="tabpanel" class="tab-pane players active" id="match-' + match + '-tab-1"></div>' +
+                      '<div role="tabpanel" class="tab-pane kills" id="match-' + match + '-tab-2"></div>' +
+                      '<div role="tabpanel" class="tab-pane join-parts" id="match-' + match + '-tab-3"></div>' +
+                    '</div>' +
+                  '</div>' +
+                '</div>' +
+              '</div>' +
+            '</div>'
           );
-          $('#' + type + ' #gamestart-match-' + match).append(
+          $('#match-' + match + ' .game-info').append(
             '<tr>' +
               '<td>' + data.mapname + '</td>' +
               '<td>' + data.matchid + '</td>' +
               '<td>' + data.gametype + '</td>' +
+              '<td class="mutators"></td>' +
             '</tr>'
           );
-          $('#gameinfo').append(
-            '<li>' +
-              '<h4>Match ' + match + '</h4>' +
-              '<table class="table" id="gameinfo-match-' + match + '">' +
+          $('#match-' + match + ' .join-parts').append(
+            '<table class="table match-join-parts">' +
+              '<thead>' +
                 '<tr>' +
-                  '<th>Info</th>' +
-                '</tr>' +
-              '</table>' +
-            '</li>'
-          );
-          $('#gameinfoend').append(
-            '<li>' +
-              '<h4>Match ' + match + '</h4>' +
-              '<table class="table" id="gameinfoend-match-' + match + '">' +
-                '<tr>' +
-                  '<th>Info</th>' +
-                '</tr>' +
-              '</table>' +
-            '</li>'
-          );
-          $('#join').append(
-            '<li>' +
-              '<h4>Match ' + match + '</h4>' +
-              '<table class="table" id="join-match-' + match + '">' +
-                '<tr>' +
+                  '<th>Type</th>' +
                   '<th>Slot</th>' +
                   '<th>IP</th>' +
                   '<th>Nickname</th>' +
                   '<th>Player ID</th>' +
-                '</tr>' +
-              '</table>' +
-            '</li>'
-          );
-          $('#team').append(
-            '<li>' +
-              '<h4>Match ' + match + '</h4>' +
-              '<table class="table" id="team-match-' + match + '">' +
-                '<tr>' +
-                  '<th>Player ID</th>' +
-                  '<th>Nickname</th>' +
                   '<th>Team ID</th>' +
                 '</tr>' +
-              '</table>' +
-            '</li>'
+              '</thead>' +
+            '</table>'
           );
-          $('#kill').append(
-            '<li>' +
-              '<h4>Match ' + match + '</h4>' +
-              '<table class="table" id="kill-match-' + match + '">' +
+          $('#match-' + match + ' .kills').append(
+            '<table class="table match-kills">' +
+              '<thead>' +
                 '<tr>' +
                   '<th>Kill Type</th>' +
                   '<th>Killer</th>' +
@@ -88,59 +75,54 @@ $(function() {
                   '<th>Attack Position</th>' +
                   '<th>Victim Items</th>' +
                 '</tr>' +
-              '</table>' +
-            '</li>'
+              '</thead>' +
+            '</table>'
           );
-          $('#part').append(
-            '<li>' +
-              '<h4>Match ' + match + '</h4>' +
-              '<table class="table" id="part-match-' + match + '">' +
-                '<tr><th>Player ID</th></tr>' +
-              '</table>' +
-            '</li>'
-          );
-          $('#players').append(
-            '<li>' +
-              '<h4>Match ' + match + '</h4>' +
-              '<table class="table" id="players-match-' + match + '">' +
+          $('#match-' + match + ' .players').append(
+            '<table class="table match-players">' +
+              '<thead>' +
                 '<tr>' +
                   '<th>Player ID</th>' +
+                  '<th>Team ID</th>' +
                   '<th>Nickname</th>' +
                   '<th>Kills</th>' +
                   '<th>Deaths</th>' +
                 '</tr>' +
-              '</table>' +
-            '</li>'
+              '</thead>' +
+            '</table>'
           );
           break;
         case 'gameinfo':
-          $('#' + type + " #gameinfo-match-" + match).append('<tr><td>' + JSON.stringify(data.mutators) + '</td></tr>');
+          $('#match-' + match + ' .game-info .mutators').append('<tr><td>' + JSON.stringify(data.mutators) + '</td></tr>');
           break;
         case 'gameinfoend':
-          $('#' + type + ' #gameinfoend-match-' + match).append('<tr><td>!!</td></tr>');
+          //$('#match-' + match + ' .game-info').append('<tr><td>!!</td></tr>');
           break;
         case 'join':
-          players[data.id] = { 'nickname': data.nickname, 'kills': 0, 'deaths': 0, 'team': 0 };
-          $('#' + type + ' #join-match-' + match).append(
-            '<tr>' +
-              '<td>' + data.slot + '</td>' +
-              '<td>' + data.ip + '</td>' +
-              '<td>' + data.nickname + '</td>' +
-              '<td>' + data.id + '</td>' +
-            '</tr>'
-          );
+          players[data.id] = {
+            'slot': data.slot,
+            'ip': data.ip,
+            'nickname': data.nickname,
+            'kills': 0,
+            'deaths': 0,
+            'team': 0
+          };
           break;
         case 'team':
           players[data.id].team = data.team;
-          $('#' + type + ' #team-match-' + match).append(
+          $('#match-' + match + ' .match-join-parts').append(
             '<tr>' +
-              '<td>' + data.id + '</td>' +
+              '<td>Join</td>' +
+              '<td>' + players[data.id].slot + '</td>' +
+              '<td>' + players[data.id].ip + '</td>' +
               '<td>' + players[data.id].nickname + '</td>' +
-              '<td>' + data.team + '</td>' +
+              '<td>' + data.id + '</td>' +
+              '<td class="team">' + players[data.id].team + '</td>' +
             '</tr>'
           );
-          $('#players #players-match-' + match).append(
+          $('#match-' + match + ' .match-players').append(
             '<tr id="' + match + '-player-' + data.id + '">' +
+              '<td>' + players[data.id].team + '</td>' +
               '<td>' + data.id + '</td>' +
               '<td>' + players[data.id].nickname + '</td>' +
               '<td class="kills">' + players[data.id].kills + '</td>' +
@@ -151,7 +133,7 @@ $(function() {
         case 'kill':
           players[data['killer-id']].kills++;
           players[data['victim-id']].deaths++;
-          $('#' + type + ' #kill-match-' + match).append(
+          $('#match-' + match + ' .match-kills').append(
             '<tr>' +
               '<td>' + data.killtype + '</td>' +
               '<td>' + players[data['killer-id']].nickname + '</td>' +
@@ -171,9 +153,14 @@ $(function() {
           }, 1000);
           break;
         case 'part':
-          $('#' + type + ' #part-match-' + match).append(
+          $('#match-' + match + ' .match-join-parts').append(
             '<tr>' +
+              '<td>Part</td>' +
+              '<td>' + players[data.id].slot + '</td>' +
+              '<td>' + players[data.id].ip + '</td>' +
               '<td>' + players[data.id].nickname + '</td>' +
+              '<td>' + data.id + '</td>' +
+              '<td>' + players[data.id].team + '</td>' +
             '</tr>'
           );
           break;
